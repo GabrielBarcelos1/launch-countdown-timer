f1aa619cc15096761a720b29b76f1aa2f9f15096
gabriel barcelos <gabrielvieirabarcelos02@gmail.com>
2021-01-31 19:55:25 -0300

    adicionando funcçoes para as contagens

--- launch-timer/src/components/timer/Timer.jsx
+++ launch-timer/src/components/timer/Timer.jsx
@@ -1,24 +1,39 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './index.css'
 
 function Timer({text, propsNumber}) {
+    useEffect(()=>{
+        setTimeout(changeNumber(text),1000);
+    },[])
 
     const [flip,setFlip] = useState("")
-    const [number, setNumber] = useState(propsNumber)
+    const [numberSeconds, setNumberSeconds] = useState(propsNumber)
+    const [numberMinutes, setNumberMinutes] = useState(propsNumber)
+    const [numberHours, setNumberHours] = useState(propsNumber)
+    const [numberDays, setNumberDays] = useState(propsNumber)
     function flipcard(){
         setFlip("flip")
         setTimeout(unFlipCard,500);
     }
-    function changeNumber(){
-        flipcard()
-        setNumber(Number(number)+1)
+    function changeNumber(type){
+        if(type === "SECONDS"){
+            flipcard()
+            setNumberSeconds(Number(numberSeconds)-1)
+        } else if(type === "MINUTES") {
+            flipcard()
+            setNumberMinutes(Number(numberMinutes)-1)
+        }
     }
     function unFlipCard(){
         setFlip("")
     }
     return(
         <div className="majorBox">
-            <span>{number}</span>
+            <span>{text === 'SECONDS' ?numberSeconds
+                :text === 'MINUTES' ? numberMinutes
+                :text === 'HOURS' ? numberHours
+                :text === 'DAYS' ?  numberDays
+                : ""}</span>
         <div className="date">
             <div className={`${flip} support`}>
             <div className= {`partTop`}>
@@ -27,7 +42,6 @@ function Timer({text, propsNumber}) {
             </div>
             </div>
         </div>
-        <button onClick={()=>changeNumber()}>aaa</button>
         <p>{text}</p>
         </div>
 
